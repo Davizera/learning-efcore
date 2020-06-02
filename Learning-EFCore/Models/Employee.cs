875ffa35179f023392a7784a2ac30590ab28d3db
Davi Alves <davi.sousa10@gmail.com>
2020-06-01 22:40:57 -0300

    Creating project, just adding the class the will serve as data sample to db migration and aadding some nuget packages

---
+++ Learning-EFCore/Models/Employee.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Learning_EFCore.Models
+{
+    public class Employee
+    {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "ID")]
+        public int ID { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "FirstName")]
+        public string FirstName { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "LastName")]
+        public string LastName { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "Phone")]
+        public string Phone { get; set; }
+
+        [Display(Name = "Title")]
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "Prefix")]
+        public string Prefix { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "Position")]
+        public string Position { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "BirthDate")]
+        public DateTime? BirthDate { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "HireDate")]
+        public DateTime? HireDate { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "Notes")]
+        public string Notes { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "Email")]
+        public string Email { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "Address")]
+        public string Address { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "City")]
+        public string City { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "Tasks")]
+        public ICollection<EmployeeTask> Tasks { get; set; }
+
+        [Display(Name = "State")]
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "StateID")]
+        public int? StateID { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "State")]
+        public string State { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "HomePhone")]
+        public string HomePhone { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "Skype")]
+        public string Skype { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "Picture")]
+        public string Picture { get; set; }
+    }
+
+    public class EmployeeTask
+    {
+        public int ID { get; internal set; }
+        public string Subject { get; internal set; }
+        public DateTime StartDate { get; internal set; }
+        public DateTime DueDate { get; internal set; }
+        public string Status { get; internal set; }
+        public object Priority { get; internal set; }
+        public int Completion { get; internal set; }
+    }
+}
